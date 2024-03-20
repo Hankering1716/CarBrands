ed068e72a7abf37e3476a98427921f65a6ed397a
Hankering1716 <116718014+Hankering1716@users.noreply.github.com>
2024-03-20 20:47:11 +0100

    Update scripts.js

--- code/scripts.js
+++ code/scripts.js
@@ -1,6 +1,6 @@
 // Get the modal
 var modal = document.getElementById("myModal");
-
+var modalMessage = document.getElementById("modalMessage")
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 // var img = $(".myImg");
 var modalImg = document.getElementById("img01");
@@ -15,15 +15,49 @@ function image(event)  {
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+const para = document.createElement("p");
+para.innerHTML = "You turned off the Modal image of car!";
+para.style.position = "fixed";
+para.style.top = "50%"
+para.style.border = "1px solid green"
+para.style.fontSize = "1vmax"
+para.style.marginLeft = "5%"
 // When the user clicks on <span> (x), close the modal
+
+document.onkeydown = function (esc) {
+    if (esc.keyCode == 27||modal.style.display=="block") {
+        
+        // Escape key pressed
+        modal.style.display = "none";
+
+        setTimeout(() => {
+            para.style.display = "none"
+        }, 1000); //
+      
+        if(para.style.display=="none") {
+            return para.style.display = "block"
+        }
+    }
+    
+};
+
+
 span.onclick = function () {
     modal.style.display = "none";
+    document.body.appendChild(para);
+
+
+   // optional functions to info user about that he got closed modalImg
+    setTimeout(() => {
+        para.style.display = "none"
+    }, 1000); //
+  
+    if(para.style.display=="none") {
+        return para.style.display = "block"
+    }
+
+
+    
 }
 
 // When the user clicks ESC on keyboard, close the modal
-document.onkeydown = function (esc) {
-    if (esc.keyCode == 27) {
-        // Escape key pressed
-        modal.style.display = "none";
-    }
-};
